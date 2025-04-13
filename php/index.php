ae2aa2238b4b9916df9e14d68fc66e52bafd2643
Kevin Bulteel <kb@koalasoft.de>
2025-04-13 18:45:47 +0200

    chore

--- php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once 'vendor/autoload.php';
-
-include_once __DIR__ . '/src/env.php';
-include_once __DIR__ . '/src/database.php';
-include_once __DIR__ . '/src/router.php';
-include_once __DIR__ . '/src/ssr.php';
-include_once __DIR__ . '/src/static_files.php';
