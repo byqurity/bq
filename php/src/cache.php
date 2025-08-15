14a2c52c4a649212f6acdab66f4b425db7edbbc0
Kevin Bulteel <kb@koalasoft.de>
2025-08-15 16:40:07 +0200

    feat: cache

--- php/src/cache.php
+++ php/src/cache.php
@@ -1,24 +1,31 @@
 <?php
 
-function cache(string $key, callable $fn, ?int $ttl = null) {
+function cache(string $key, callable $fn, ?int $ttl = null, ?callable $status = null) {
   $encoding  = $_ENV['CACHE_TYPE'] ?? 'serialized';
   $directory = sys_get_temp_dir(). '/cache';
   $file      = $directory . '/' . $key . '.' . $encoding;
   $ttl     ??= (int) $_ENV['CACHE_TTL'] ?? 0;
   $data      = null;
+  $status  ??= fn($status) => error_log('Cache-Status: ' . $status . ' [' . $key . ']');
 
   if (!is_dir($directory)) {
     mkdir($directory);
   }
 
-  if ($ttl == 0 || !is_file($file) || (time() - filemtime($file)) < $ttl) {
+  if ($ttl === 0 || !is_file($file) || (time() - filemtime($file)) < $ttl) {
     $data = $fn();
+
+    $status(!is_file($file) ? 'MISS' : 'REVALIDATED');
 
     if ($ttl > 0) {
       file_put_contents($file, $encoding == 'json' ? json_encode($data) : serialize($data));
+    } else {
+      $status('BYPASS');
     }
 
   } else {
+    $status('HIT');
+
     $raw  = file_get_contents($file);
     $data = $encoding == 'json' ? json_decode($raw) : unserialize($raw);
   }
